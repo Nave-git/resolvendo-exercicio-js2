fb9381a77c8b45372df7f3feaae43bbe9e6dfed1
Nave-git <bianchiifpe@gmail.com>
2024-10-25 20:13:57 -0300

    Add files via upload

---
+++ Aulas-avan-adas-JS-master/Js/exercicios.js
@@ -0,0 +1,119 @@
+// Usando os operadores necessários e as condicionais “if, eles e eles if” Responda as questões de 1 até 10: 
+
+// 1)Verifique se um número é positivo ou negativo 
+// let numero = parseInt(prompt("digit um valor"));
+
+
+// if (numero < 0) {
+//     document.write("Número é negativo")
+//     console.log("Número é negativo")
+//     alert("Número é negativo")
+// } else {
+//     document.write("Numero é positivo")
+//     console.log("Numero é positivo")
+//     alert("Numero é positivo")
+// }
+// var numero = 10;
+
+//  let resultado = numero < 0 ? "Número é negativo" : "Numero é positivo"
+
+// document.write(resultado)
+// console.log(resultado)
+//  alert(resultado)
+// 2)Verifique se um número é par ou ímpar:
+// let numero = parseInt(prompt("digit um valor"));
+
+// if(numero % 2 === 0){
+    
+//     document.write("Este número é par")
+//     console.log("Este número é par")
+//     alert("Este número é par")
+
+// }else{
+
+//     document.write("Este número é impar")
+//     console.log("Este número é impar")
+//     alert("Este número é impar")
+// }
+
+// let resultado = numero % 2 === 0 ? "Este número é par" : "Este número é impar"
+
+// document.write(resultado)
+// 3)Classifique uma nota de 0 a 10, da seguinte forma: se for igual ou maior que 9 a nota é “A”, se for igual a 8 nota “B”, se for igual a 7  nota “C”, se for igual a 6 nota “D”, se for igual a 5 ou menor, nota “F”.
+// let nota = parseInt(prompt("digit a nota"));
+
+// if(nota === 10 || nota === 9){
+//     document.write("O conceito é 'A' ")
+//     console.log("O conceito é 'A' ")
+//     alert("O conceito é 'A' ")
+// }else if(nota === 8){
+//     document.write("O conceito é 'B' ")
+//     console.log("O conceito é 'B' ")
+//     alert("O conceito é 'B' ")
+// }else if(nota === 7){
+//     document.write("O conceito é 'C' ")
+//     console.log("O conceito é 'C' ")
+//     alert("O conceito é 'C' ")
+// }else if(nota === 6){
+//     document.write("O conceito é 'D' ")
+//     console.log("O conceito é 'D' ")
+//     alert("O conceito é 'D' ")
+// }else{
+//     document.write("O conceito é 'E' ")
+//     console.log("O conceito é 'E' ")
+//     alert("O conceito é 'E' ")
+// }
+// 4) Verifique se um ano é bissexto:
+// let ano =  parseInt(prompt("digite um ano: "));
+
+//  if((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)){
+//     document.write("O ano é bissexto")
+//  }else{
+//     document.write("O ano não é bissexto")
+//  }
+
+//  calendario = (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0) ?
+//   "O ano é bissexto" : "O ano não é bissexto"
+
+//   document.write(calendario)
+// 5)Verifique se um número está entre 10 e 20:
+// let numero =  parseInt(prompt("digite um número: "));
+
+// if(numero >= 10 && numero <= 20){
+//  document.write("Este número esta entre 10 e 20.")
+// }else{
+//     document.write("Este número 'Não' esta entre 10 e 20.")
+// }
+// 6)Determine o maior de dois números:
+// let numero1 =  parseInt(prompt("digite o prímeiro: "));
+// let numero2 =  parseInt(prompt("digite o segundo: "));
+
+// if(numero1 > numero2){
+//  document.write(" O valor: " + numero1 + " é maior que o valor " + numero2)
+// }else
+// document.write(" O valor: " + numero2 + " é maior que o valor " + numero1)
+// 7)Verifique se um caractere é uma vogal ou consoante:
+
+let alfabeto = prompt("DIgite uma letra: ")
+
+if((alfabeto === 'a') || (alfabeto ==='e')||(alfabeto ==='i')||(alfabeto ==='o')||(alfabeto ==='u') ){
+ document.write("A letra '" + alfabeto + "' é uma vogal!")
+}else{
+    document.write("A letra '" + alfabeto + "' é uma consoante!")
+
+}
+
+// 8)Verifique se um número é múltiplo de 3 ou 5:
+
+// 9)Determine a fase do dia com base na hora (0-23):
+
+// 10)Verifique se uma pessoa é maior de idade:
+
+// 11) Use Switch case para verificar o dia da semana (0 - Domingo, 1 - Segunda, etc.):
+
+// 12) Use Switch case para classificar uma fruta:
+// 13) Use Operador lógico AND (&&) para verificar se um número está entre 10 e 20:
+// 14) Use o operador ternário para verificar se uma pessoa pode votar (maior de 18 anos):
+// 15) Use Operador lógico OR (||) para verificar se uma pessoa é criança ou idosa:
+// 16) Use Switch case para verificar a estação do ano com base no mês:
+// 17) Use Operador ternário encadeado para classificar uma nota entre 0 e 10, da seguinte forma: se for igual ou maior que 9 a nota é “A”, se for igual a 8 nota “B”, se for igual a 7  nota “C”, se for igual a 6 nota “D”, se for igual a 5 ou menor, nota “F”. 
